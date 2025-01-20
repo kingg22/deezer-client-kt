e7b7d8f0e1d0f811a6cfcf4e878409ccffe5f6f2
Kingg22 <89825425+Kingg22@users.noreply.github.com>
2025-01-20 17:39:30 -0500

    build: dokka v2 enabled and add testing dependencies

--- deezer-sdk/build.gradle.kts
+++ deezer-sdk/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinPluginSerialization)
+    alias(libs.plugins.dokka)
 }
 
 group = "io.github.kingg22"
@@ -32,7 +33,7 @@ kotlin {
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
-            implementation(libs.ktor.engine.okhttp)
+            implementation(libs.bundles.testing)
         }
     }
 }
