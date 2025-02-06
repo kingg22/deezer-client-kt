7df41f6ea5ab77e35a0aad49466f04ee387124da
Rey <89825425+Kingg22@users.noreply.github.com>
2025-02-05 19:47:59 -0500

    feat(gw): add alternative endpoint of track
    test: enhance performance of removeGarbage function

--- deezer-sdk/src/commonTest/kotlin/io/github/kingg22/deezerSdk/KtorEngineMocked.kt
+++ deezer-sdk/src/commonTest/kotlin/io/github/kingg22/deezerSdk/KtorEngineMocked.kt
@@ -133,6 +133,8 @@ object KtorEngineMocked {
             readResourceFile("/gw/responses/get_album_discography.json")
         "/ajax/gw-light.php/.?api_token=$GW_TOKEN&method=song.getListData&api_version=1.0&input=3" ->
             readResourceFile("/gw/responses/get_songs_data.json")
+        "/ajax/gw-light.php/.?api_token=$GW_TOKEN&method=deezer.pageTrack&api_version=1.0&input=3" ->
+            readResourceFile("/gw/responses/get_track_page.json")
 
         // Media API
         "/v1/get_url" -> readResourceFile("/media/responses/get_medias.json")
