159870326aa05908532062f6fabab6ed6236fb05
Kingg22 <89825425+Kingg22@users.noreply.github.com>
2025-02-28 17:00:40 -0500

    fix: add missing imports

--- deezer-sdk/src/commonTest/kotlin/io/github/kingg22/deezerSdk/api/DeezerApiClientTest.kt
+++ deezer-sdk/src/commonTest/kotlin/io/github/kingg22/deezerSdk/api/DeezerApiClientTest.kt
@@ -10,6 +10,7 @@ import io.github.kingg22.deezerSdk.utils.HttpClientBuilder.Companion.httpClientB
 import io.kotest.assertions.json.shouldEqualJson
 import kotlinx.coroutines.test.runTest
 import kotlinx.datetime.LocalDateTime
+import kotlin.jvm.JvmStatic
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
