2788c1fa19b96fea8148bdf7460775a38190ba27
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-27 15:20:47 -0500

    build(project): add gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.kotlinxKover) apply false
+    alias(libs.plugins.kotlinxResources) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.ktorfit) apply false
+}
