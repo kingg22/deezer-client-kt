99ecc5fc0b817afc949a2842bae2d5e2246561a0
Rey <89825425+Kingg22@users.noreply.github.com>
2025-08-03 18:26:47 -0500

    perf(deezer-client-kt): BREAKING error class now is internal

--- deezer-client-kt/src/commonMain/kotlin/io/github/kingg22/deezer/client/api/objects/ErrorContainer.kt
+++ deezer-client-kt/src/commonMain/kotlin/io/github/kingg22/deezer/client/api/objects/ErrorContainer.kt
@@ -9,7 +9,7 @@ import kotlinx.serialization.Serializable
  * @see <a href="https://developers.deezer.com/api/errors">Deezer Errors</a>
  */
 @Serializable
-data class ErrorContainer(val error: ErrorDetail) {
+internal class ErrorContainer(val error: ErrorDetail) {
     @Serializable
-    data class ErrorDetail(val type: String, val message: String? = null, val code: Int)
+    internal class ErrorDetail(val type: String, val message: String? = null, val code: Int)
 }
