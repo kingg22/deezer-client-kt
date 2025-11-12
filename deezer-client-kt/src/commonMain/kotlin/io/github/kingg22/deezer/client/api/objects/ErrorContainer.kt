2199c58782b90370cb6e48a96d20b894887bf882
Rey <89825425+kingg22@users.noreply.github.com>
2025-11-11 22:39:54 -0500

    perf(deezer-client-kt): mark ErrorContainer as internal
    Is internal visibility, but java can access this bypassing internal, mark internal to docs

--- deezer-client-kt/src/commonMain/kotlin/io/github/kingg22/deezer/client/api/objects/ErrorContainer.kt
+++ deezer-client-kt/src/commonMain/kotlin/io/github/kingg22/deezer/client/api/objects/ErrorContainer.kt
@@ -1,5 +1,6 @@
 package io.github.kingg22.deezer.client.api.objects
 
+import io.github.kingg22.deezer.client.utils.InternalDeezerClient
 import kotlinx.serialization.Serializable
 
 /**
@@ -9,7 +10,9 @@ import kotlinx.serialization.Serializable
  * @see <a href="https://developers.deezer.com/api/errors">Deezer Errors</a>
  */
 @Serializable
+@InternalDeezerClient
 internal class ErrorContainer(val error: ErrorDetail) {
     @Serializable
+    @InternalDeezerClient
     internal class ErrorDetail(val type: String, val message: String? = null, val code: Int)
 }
