e99e239a8d121ab08998f339ec8ac83217743245
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-02 15:06:09 -0500

    refactor(deezer-client-kt): add Resource reload overload with a client
    Remove helper ext function, generate without default compatibility (this is the first step to the next major breaking change)

--- deezer-client-kt/src/commonMain/kotlin/io/github/kingg22/deezer/client/api/objects/Resource.kt
+++ deezer-client-kt/src/commonMain/kotlin/io/github/kingg22/deezer/client/api/objects/Resource.kt
@@ -1,9 +1,9 @@
 package io.github.kingg22.deezer.client.api.objects
 
+import io.github.kingg22.deezer.client.api.DeezerApiClient
 import io.github.kingg22.deezer.client.exceptions.DeezerApiException
 import io.github.kingg22.deezer.client.utils.AfterInitialize
 import kotlin.coroutines.cancellation.CancellationException
-import kotlin.jvm.JvmSynthetic
 
 /**
  * _Internal_ Represents a Resource of [Deezer API](https://developers.deezer.com/api/).
@@ -11,13 +11,32 @@ import kotlin.jvm.JvmSynthetic
  * Inspired in [Deezer Python Resource](https://github.com/browniebroke/deezer-python/blob/06065d357be3761e895e3605fdc0619783685b7c/src/deezer/resources/resource.py)
  * @author Kingg22
  */
+@JvmDefaultWithoutCompatibility
 interface Resource {
     val id: Long
     val type: String
 
-    /** Reloads the current resource from the API, getting all of its full properties if it was initially obtained partially, or it's outdated */
+    /**
+     * Reloads the current resource from the API,
+     * getting all of its full properties if it was initially obtained partially, or it's outdated
+     * @param client The Deezer API client to use
+     */
+    @JvmSynthetic
+    @Throws(DeezerApiException::class, CancellationException::class)
+    suspend fun reload(client: DeezerApiClient): Resource
+
+    /**
+     * Reloads the current resource from the API,
+     * getting all of its full properties if it was initially obtained partially, or it's outdated
+     */
+    @Suppress("DEPRECATION")
+    @Deprecated(
+        "Use reload(client: DeezerApiClient) instead, pass a client explicitly",
+        ReplaceWith("reload(client)"),
+        level = DeprecationLevel.WARNING,
+    )
     @AfterInitialize
     @JvmSynthetic
     @Throws(DeezerApiException::class, CancellationException::class)
-    suspend fun reload(): Resource
+    suspend fun reload(): Resource = reload(io.github.kingg22.deezer.client.api.GlobalDeezerApiClient.requireInstance())
 }
