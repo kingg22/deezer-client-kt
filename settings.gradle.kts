25bc6751c9ebd19033af24f18a6dae704ee766a2
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-21 14:39:33 -0500

    build(gradle): change repositoriesMode to prefer because going to use yarn and npm

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google {
             mavenContent {
